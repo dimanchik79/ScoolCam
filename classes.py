fe35380d0fec5764bece15722caa63f3eb0682bb
dimanchik79 <dimanchik.dmitri@yandex.ru>
2024-05-24 13:20:27 +0700

    add audio record

--- classes.py
+++ classes.py
@@ -1,6 +1,35 @@
+import cv2
 import pyaudio
 import wave
+import numpy as np
 import threading
+
+class VideoCapture:
+    def __init__(self, cam_number: int):
+        self.cam_number = cam_number
+        self.cap = cv2.VideoCapture(self.cam_number)
+        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        self.out = cv2.VideoWriter('camera_0.avi', self.fourcc, 25.0, (640, 480))
+        self.audio = AudioRecorder()
+
+    def video_save(self):
+        self.cap.set(cv2.CAP_PROP_FPS, 30)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+        while True:
+            ret, frame = self.cap.read()
+            self.out.write(frame)
+            self.audio.record()
+
+            cv2.imshow("Camera-0", frame)
+            if cv2.waitKey(1) & 0xFF == 27:
+                break
+
+        self.audio.stop()
+        self.out.release()
+        self.cap.release()
+        cv2.destroyAllWindows()
 
 
 class AudioRecorder:
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from video import *
+from classes import *
 import subprocess
 import os
 
 
 def merge_audio_video():
-    cmd = "ffmpeg -i temp_audio.wav -i temp_video.avi -c:v copy -c:a aac output.mp4"
+    cmd = "ffmpeg -i temp_audio.wav -i temp_video.avi -c:v copy -c:a aac camera_0.avi"
     subprocess.call(cmd, shell=True)
-    os.remove("temp_audio.wav")
-    os.remove("temp_video.avi")
+
+    # os.remove("temp_audio.wav")
+    # os.remove("temp_video.avi")
 
 
 def start():
