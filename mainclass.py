a16aa0767b0cbc678e7f55774f0624e5afe9d5bc
dimanchik79 <dimanchik.dmitri@yandex.ru>
2024-05-30 21:09:13 +0700

    add label check

--- mainclass.py
+++ mainclass.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
-from PyQt5 import uic, QtGui, QtWidgets
-from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QMainWindow, QTableWidget
+from PyQt5 import uic, QtCore
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QLabel, QCheckBox
 from typing_extensions import Dict
 
 from config import SCROLL_BAR_VERTICAL, SCROLL_BAR_HORIZONTAL, LABEL_RED, LABEL_GREEN
@@ -28,8 +28,9 @@ class StartWindow(QMainWindow):
         super(StartWindow, self).__init__()
         uic.loadUi("GUI/main.ui", self)
         self.setFixedSize(1120, 756)
+
         self.message = []
-        self.set_message("Добро пожаловать в ScoolCam!!!")
+        self.check_list = [0] * 6
 
         self.cameras = cameras
         self.microphones = microphones
@@ -41,20 +42,49 @@ class StartWindow(QMainWindow):
         self.find_cams.clicked.connect(self.add_cams)
         self.auditory_line.textChanged.connect(self.auditory)
 
+        self.set_message("Добро пожаловать в ScoolCam!!!")
+
     def auditory(self):
         """Метод ввода аудитории"""
         if self.auditory_line.text() != "":
-            self.chek2.setStyleSheet(LABEL_GREEN)
+            self.check_list[1] = 1
+            self.check2.setStyleSheet(LABEL_GREEN)
         else:
-            self.chek2.setStyleSheet(LABEL_RED)
+            self.check_list[1] = 0
+            self.check2.setStyleSheet(LABEL_RED)
 
     def add_cams(self):
         """Метод добавления активных камер в таблицу"""
+        self.set_message("Ждите... Идет подключение активных камер.")
         self.cam_table.setColumnCount(3)
         self.cam_table.setRowCount(len(self.cameras))
-        self.cam_table.setHorizontalHeaderLabels(["Выбор", "Предпросмотр", "Название"])
+        self.cam_table.setHorizontalHeaderLabels(["Выбор", "Предпросмотр", "Название камеры"])
         for index in range(1, 3):
             self.cam_table.horizontalHeaderItem(index).setTextAlignment(Qt.AlignHCenter)
+
+        for row in range(len(self.cameras)):
+            self.cam_table.setRowHeight(row, 200)
+            self.cam_table.setColumnWidth(0, 55)
+            self.cam_table.setColumnWidth(1, 200)
+            self.cam_table.setColumnWidth(2, 250)
+
+            # Установка items для 3й колонки
+            item = QTableWidgetItem(self.cameras[row])
+            item.setTextAlignment(2)
+            item.setFlags(item.flags() & ~-1)
+            self.cam_table.setItem(row, 2, item)
+
+            item = QTableWidgetItem("")
+            item.setFlags(QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled)
+            item.setCheckState(QtCore.Qt.Unchecked)
+            item.setFlags(item.flags() & ~1)
+            self.cam_table.setItem(row, 0, item)
+
+            label = QLabel(str(row), alignment=132)
+            self.cam_table.setCellWidget(row, 1, label)
+
+        self.check_list[0] = 1
+        self.check1.setStyleSheet(LABEL_GREEN)
 
     def clear_console(self):
         """Метод очищает консоль"""
